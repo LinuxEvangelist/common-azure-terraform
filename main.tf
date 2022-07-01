f96f3cc6e12efc6ca8c11add4a566418d972ac83
Ricardo Miguel Llontop Garcia <llontopg.ricardo@gmail.com>
2022-07-01 12:20:22 -0500

    basic structure

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "= 2.91.0"
+    }
+  }
+  required_version = "~> 1.2.2"
+}
+
+
+module "az_resource_group" {
+  source = "./modules/resource_group"
+}
+module "az_storage" {
+  source              = "./modules/storage"
+  resource_group_name = module.az_resource_group.resource_group_name
+  location            = module.az_resource_group.resource_group_location
+}
